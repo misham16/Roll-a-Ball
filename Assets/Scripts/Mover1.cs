bac5f40ff743dc65edd1f814ef77495d904fa052
misham16 <mmclendon@knights.ucf.edu>
2019-07-09 17:41:53 -0400

    Complete Tiers

---
+++ Assets/Scripts/Mover1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover1 : MonoBehaviour
+{
+    Vector3 startA = new Vector3(58.0f, 0.5f, 5.0f);
+    Vector3 endA = new Vector3(42.0f, 0.5f, 5.0f);
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(startA, endA, Mathf.PingPong(Time.time, 1));
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,23 @@ public class PlayerController : MonoBehaviour
 public float speed;
 public Text countText;
 public Text winText;
+public Text scoreText;
+public Text livesText;
 
 private Rigidbody rb;
 private int count;
+private int score;
+private int lives;
 
 void Start ()
 {
     rb = GetComponent<Rigidbody>();
     count = 0;
+    score = 0;
+    lives = 3;
     SetCountText ();
+    SetScoreText ();
+    SetLivesText ();
     winText.text = "";
 }
     void FixedUpdate ()
@@ -38,15 +46,49 @@ void Start ()
         {
             other.gameObject.SetActive(false);
             count = count + 1;
+            score = score + 1;
             SetCountText ();
+            SetScoreText ();
+            SetLivesText ();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score - 1;
+            lives = lives - 1;
+            SetCountText ();
+            SetScoreText ();
+            SetLivesText ();
+        }
+         if (count == 12)
+        {
+            transform.position = new Vector3(50, transform.position.y, 0);
         }
     }
     void SetCountText ()
     {
-        countText.text = "Count: " + count.ToString ();
-        if (count >= 12)
+        countText.text = "Total: " + count.ToString ();
+        if (count >= 25)
         {
-            winText.text = "You Win!";
+            winText.text = "Your score is " + score.ToString ();
+        }
+    }
+    void SetScoreText ()
+    {
+        scoreText.text = "Score: " + score.ToString ();
+        if (score == 24)
+        {
+            winText.text = "You Win!! ";
+        }
+    }
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+        if (lives == 0)
+        {
+            gameObject.SetActive(false);
+            winText.text = "GAME OVER";
         }
     }
 }
